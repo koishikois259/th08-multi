db3d7e88fc41794a283a1edd47b3194d4ea2ecad
toadster172 <toadster172@gmail.com>
2025-03-29 22:16:06 -0500

    Prioritize win32 headers over DirectX headers

--- scripts/th08vars.bat
+++ scripts/th08vars.bat
@@ -8,7 +8,7 @@
 @SET CYGWIN_PATH=%DEVENV_PREFIX%\cygwin\bin
 
 @SET PATH=%NINJA_PATH%;%PY_PATH%;%MS_VC_PATH%\Bin;%VS_BASE_PATH%\FrameworkSDK\Bin;%VS_BASE_PATH%\Common7\IDE;%CYGWIN_PATH%;%PATH%
-@SET INCLUDE=%DXSDK_DIR%\include;%MS_VC_PATH%\INCLUDE;%MS_VC_PATH%\PlatformSDK\include\prerelease;%MS_VC_PATH%\PlatformSDK\include;%MS_VC_PATH%\PlatformSDK\common\include\prerelease;%MS_VC_PATH%\PlatformSDK\common\include
-@SET LIB=%DXSDK_DIR%\lib;%MS_VC_PATH%\LIB;%MS_VC_PATH%\PlatformSDK\lib\prerelease;%MS_VC_PATH%\PlatformSDK\lib;%MS_VC_PATH%\PlatformSDK\common\lib\prerelease;%MS_VC_PATH%\PlatformSDK\common\lib;%MS_VC_PATH%\PlatformSDK\x86\lib
+@SET INCLUDE=%MS_VC_PATH%\INCLUDE;%MS_VC_PATH%\PlatformSDK\include\prerelease;%MS_VC_PATH%\PlatformSDK\include;%MS_VC_PATH%\PlatformSDK\common\include\prerelease;%MS_VC_PATH%\PlatformSDK\common\include;%DXSDK_DIR%\include
+@SET LIB=%MS_VC_PATH%\LIB;%MS_VC_PATH%\PlatformSDK\lib\prerelease;%MS_VC_PATH%\PlatformSDK\lib;%MS_VC_PATH%\PlatformSDK\common\lib\prerelease;%MS_VC_PATH%\PlatformSDK\common\lib;%MS_VC_PATH%\PlatformSDK\x86\lib;%DXSDK_DIR%\lib
 
 @ECHO You have entered Touhou 08 devenv from %DEVENV_PREFIX%, have fun~!
