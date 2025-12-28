a2c4da94b79e6aaf5f847e935bc8dab852cbc9fc
xmvziron <103202084+xmvziron@users.noreply.github.com>
2025-12-28 18:07:29 +0100

    Add missing structs for SprtCtrl

--- src/SprtCtrl.cpp
+++ src/SprtCtrl.cpp
@@ -3,4 +3,17 @@
 namespace th08
 {
 DIFFABLE_STATIC(SprtCtrl *, g_SprtCtrl);
+
+void SprtCtrl::ExecuteAnms(Sprt *sprite, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        if (sprite->scriptIndex > -1)
+        {
+            g_SprtCtrl->ExecuteScript(sprite);
+        }
+        sprite++;
+    }
+}
+
 }; // Namespace th08
