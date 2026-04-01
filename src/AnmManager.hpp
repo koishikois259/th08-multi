08efd899008b679b0984a133a9b720a761bcd03c
xmvziron <103202084+xmvziron@users.noreply.github.com>
2026-04-01 22:44:40 +0200

    Initialize VM to zero

--- src/AnmManager.hpp
+++ src/AnmManager.hpp
@@ -391,6 +391,8 @@ struct AnmVm
 
     void Initialize()
     {
+        memset(this, 0, sizeof(AnmVm));
+
         this->prefix.scale.x = 1.0f;
         this->prefix.scale.y = 1.0f;
         this->prefix.color1.d3dColor = COLOR_WHITE;
