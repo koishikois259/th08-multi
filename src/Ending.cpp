9bee76da4fc792b1ee867b6af52d525fc29bd185
PhoenixWrong00 <francyterracciano08@gmail.com>
2026-05-17 19:17:32 +0200

    Matched Ending::ParseEndFile

--- src/Ending.cpp
+++ src/Ending.cpp
@@ -102,31 +102,41 @@ void Ending::FadingEffect()
     }
 }
 
-#pragma var_order(local_8, textBuffer, index)
+#pragma var_order(local_8, textBuffer, index, i, scriptIdx, vmIndex, spriteIdx, firstRead, secondRead, cmdParam, fadeTime)
 ZunResult Ending::ParseEndFile()
 {
     char textBuffer[68];
     i32 index;
     i32 local_8;
+    i32 i;
 
+    i32 vmIndex;
+    i32 scriptIdx;
+    i32 spriteIdx;
+    i32 firstRead;
+    i32 secondRead;
+    i32 cmdParam;
+    f32 fadeTime;
+
+    local_8 = false;
     index = 0;
-    local_8 = 0;
+
     memset(textBuffer, 0, sizeof(textBuffer));
 
     if(this->timer3 > 0){
         this->timer3--;
 
-        if (this->unk2a88 == 0){
-            if((WAS_PRESSED(0x1001)) || ((this->canSkip != 0 && (IS_PRESSED(0x100) != 0)))){
-                this->timer3 = 0;
-            }
+        if (this->unk2a88 != 0){
+            this->unk2a88--;
         }
         else {
-            this->unk2a88--;
+            if((WAS_PRESSED(0x1001)) || ((this->canSkip != 0 && (IS_PRESSED(0x100) != 0)))){
+              this->timer3 = 0;
+            }
         }
 
         if (this->timer3 <= 0){
-            for(i32 i = 0; i < 16; i++){
+            for(i = 0; i < 15; i++){
                 this->vms[i].SetInterrupt(2);
             }
             this->unk2a9c = 0;
@@ -138,57 +148,100 @@ ZunResult Ending::ParseEndFile()
 
     if (this->timer2 > 0) {
     this->timer2--;
-    if (this->unk2a8c == 0) {
-      if ((WAS_PRESSED(0x1001)) || ((this->canSkip != 0 && (IS_PRESSED(0x100) != 0)))) {
+    if (this->unk2a8c != 0) {
+      this->unk2a8c--;
+
+    } else {
+      if ((WAS_PRESSED(0x1001)) || ((this->canSkip != 0 && (IS_PRESSED(0x100) != 0)))){
         this->timer2 = 0;
       }
-    } else {
-      this->unk2a8c--;
     }
     goto end_of_parse; 
   }
 
-    if(this->timer3 <= 0)
-    {
-        for(;;){
+        while(true){
             switch(*this->cursorPtr){
-            case '\0':
-            case '\n':
-            case '\r':
-                if(index != 0){
-                    g_AnmManager->DrawTextLeft(
-                        &this->vms[this->unk2a9c],
-                        this->unk2aa0,
-                        COLOR_WHITE,
-                        textBuffer
-                    );
-                    this->vms[this->unk2a9c].SetInterrupt(1);
-                    while(*this->cursorPtr == '\n' || *this->cursorPtr == '\0' || *this->cursorPtr == '\r'){
-                        this->cursorPtr++;
-                    }
-                    if(IS_PRESSED(0x1001) == 0){
-                        this->timer2 = this->unk2a90;
-                        this->unk2a8c = this->unk2a94;
-                    }
-                    else {
-                        this->timer2 = this->unk2a94;
-                        this->unk2a8c = this->unk2a94;
-                    }
-                    this->unk2a9c++;
-                    goto end_of_parse;
-                }
-                break;
-
-            default:
-                textBuffer[index] = *this->cursorPtr;
-                textBuffer[index + 1] = *(this->cursorPtr + 1);
-                index += 2;
-                this->cursorPtr += 2;
-                break;
-
             case '@':
                 this->cursorPtr++;
                 switch(*this->cursorPtr){
+                case 'b':
+                    if(g_AnmManager->LoadSurface(0, (this->cursorPtr + 1))){
+                        return ZUN_ERROR;
+                    }
+                    break;
+                case 'a':
+                    this->cursorPtr++;
+                    vmIndex = this->ReadEndFileParameter();
+                    scriptIdx = this->ReadEndFileParameter();
+                    spriteIdx = this->ReadEndFileParameter();
+                    this->anmFile->ExecuteAnmIdx(&this->vms[vmIndex], scriptIdx);
+                    this->anmFile->SetSprite(&this->vms[vmIndex], spriteIdx);
+                    break;
+                case 'V':
+                    this->cursorPtr++;
+                    firstRead = this->ReadEndFileParameter();
+                    secondRead = this->ReadEndFileParameter();
+                    this->unk10 = (f32)firstRead / (f32)secondRead;
+                    break;
+                case 'v':
+                    this->cursorPtr++;
+                    this->unk0c = (f32)this->ReadEndFileParameter();
+                    break;
+                case 'F':
+                    if(this->LoadEnding(this->cursorPtr + 1) != ZUN_SUCCESS){
+                        return ZUN_ERROR;
+                    }
+                    index = 0;
+                    local_8 = 0;
+                    this->canSkip = this->unk2a5c;
+                case 'R':
+                    for(cmdParam = 0; cmdParam < 16; cmdParam++){
+                        this->vms[cmdParam].scriptIndex = 0;
+                    }
+                    break;
+                case 'm':
+                    g_Supervisor.LoadMusic(0, this->cursorPtr + 1);
+                    g_Supervisor.PlayMusic(0, 0);
+                    break;
+                case 'M':
+                    this->cursorPtr++;
+                    fadeTime = (f32)this->ReadEndFileParameter();
+                    g_Supervisor.FadeOutMusic(fadeTime);
+                    break;
+                case 's':
+                    this->cursorPtr++;
+                    this->unk2a90 = this->ReadEndFileParameter();
+                    this->unk2a94 = this->ReadEndFileParameter();
+                    break;
+                case 'c':
+                    this->cursorPtr++;
+                    this->unk2aa0 = this->ReadEndFileParameter();
+                    break;
+                case 'r':
+                    this->cursorPtr++;
+                    this->timer3 = this->ReadEndFileParameter();
+                    this->unk2a88 = this->ReadEndFileParameter();
+                    this->timer2 = 0;
+                    this->unk2a8c = 0;
+                    while(*this->cursorPtr != '\n' && *this->cursorPtr != '\r'){
+                        this->cursorPtr++;
+                    }
+                    while(*this->cursorPtr == '\n' || *this->cursorPtr == '\r'){
+                        this->cursorPtr++;
+                    }
+                    goto end_of_parse;
+                case 'w':
+                    this->cursorPtr++;
+                    this->timer2 = this->ReadEndFileParameter();
+                    this->unk2a8c = this->ReadEndFileParameter();
+                    while(*this->cursorPtr != '\n' && *this->cursorPtr != '\r'){
+                        this->cursorPtr++;
+                    }
+                    while(*this->cursorPtr == '\n' || *this->cursorPtr == '\r'){
+                        this->cursorPtr++;
+                    }
+                    goto end_of_parse;
+
                 case '0':
                     this->cursorPtr++;
                     this->fadeMode = 1;
@@ -212,90 +265,10 @@ ZunResult Ending::ParseEndFile()
                     this->fadeMode = 4;
                     this->fadeTimer = 0;
                     this->fadeDuration = this->ReadEndFileParameter();
-                    break;
-                case 'F':
-                    if(this->LoadEnding(this->cursorPtr + 1) != ZUN_SUCCESS){
-                        return ZUN_ERROR;
-                    }
-                    index = 0;
-                    local_8 = 0;
-                    this->canSkip = this->unk2a5c;
-                    // fallthrough
-                case 'R':
-                    for(i32 i = 0; i < 16; i++){
-                        this->vms[i].scriptIndex = 0;
-                    }
-                    break;
-                case 'M':
-                    this->cursorPtr++;
-                    g_Supervisor.FadeOutMusic((f32)this->ReadEndFileParameter());
-                    break;
-                case 'V':{
-                    this->cursorPtr++;
-                    i32 firstRead = this->ReadEndFileParameter();
-                    i32 secondRead = this->ReadEndFileParameter();
-                    this->unk10 = (f32)firstRead / (f32)secondRead;
-                    break;
-                }
-                case 'a':{
-                    this->cursorPtr++;
-                    i32 vmIndex = this->ReadEndFileParameter();
-                    i32 scriptIdx = this->ReadEndFileParameter();
-                    i32 spriteIdx = this->ReadEndFileParameter();
-                    this->anmFile->ExecuteAnmIdx(&this->vms[vmIndex], scriptIdx);
-                    this->anmFile->SetSprite(&this->vms[vmIndex], spriteIdx);
-                    break;
-                }
-                case 'b':
-                    if(g_AnmManager->LoadSurface(0, (this->cursorPtr + 1))){
-                        return ZUN_ERROR;
-                    }
-                    break;
-                case 'c':
-                    this->cursorPtr++;
-                    this->unk2aa0 = this->ReadEndFileParameter();
-                    break;
-                case 'm':
-                    g_Supervisor.LoadMusic(0, this->cursorPtr + 1);
-                    g_Supervisor.PlayMusic(0, 0);
-                    break;
-                case 'r':{
-                    this->cursorPtr++;
-                    i32 waitTime = this->ReadEndFileParameter();
-                    this->timer3 = waitTime;
-                    i32 unskippableFrames = this->ReadEndFileParameter();
-                    this->unk2a88 = unskippableFrames;
-                    this->timer2 = 0;
-                    this->unk2a8c = 0;
-                    while(*this->cursorPtr != '\n' && *this->cursorPtr != '\r'){
-                        this->cursorPtr++;
-                    }
-                    while(*this->cursorPtr == '\n' || *this->cursorPtr == '\r'){
-                        this->cursorPtr++;
-                    }
-                    goto end_of_parse;
-                }
-                case 's':
-                    this->cursorPtr++;
-                    this->unk2a90 = this->ReadEndFileParameter();
-                    this->unk2a94 = this->ReadEndFileParameter();
-                    break;
-                case 'v':
-                    this->cursorPtr++;
-                    this->unk0c = (f32)this->ReadEndFileParameter();
-                    break;
-                case 'w':{
-                    this->cursorPtr++;
-                    this->timer2 = this->ReadEndFileParameter();
-                    this->unk2a8c = this->ReadEndFileParameter();
-                    while(*this->cursorPtr != '\n' && *this->cursorPtr != '\r'){
-                        this->cursorPtr++;
-                    }
-                    while(*this->cursorPtr == '\n' || *this->cursorPtr == '\r'){
-                        this->cursorPtr++;
-                    }
-                    goto end_of_parse;
-                }
+                    break;                
+
+                
+                
                 case 'z':
                     return ZUN_ERROR;
                 }
@@ -306,20 +279,43 @@ ZunResult Ending::ParseEndFile()
                     this->cursorPtr++;
                 }
                 break;
+
+            case '\0':
+            case '\n':
+            case '\r':
+                if(index != 0){
+                    g_AnmManager->DrawTextLeft(
+                        &this->vms[this->unk2a9c],
+                        this->unk2aa0,
+                        COLOR_WHITE,
+                        textBuffer
+                    );
+                    this->vms[this->unk2a9c].SetInterrupt(1);
+                }
+                    while(*this->cursorPtr == '\n' || *this->cursorPtr == '\0' || *this->cursorPtr == '\r'){
+                        this->cursorPtr++;
+                    }
+                    if(IS_PRESSED(0x1001)){
+                        this->timer2 = this->unk2a94;
+                        this->unk2a8c = this->unk2a94;
+                    }
+                    else {
+                        this->timer2 = this->unk2a90;
+                        this->unk2a8c = this->unk2a94;
+                    }
+                    this->unk2a9c++;
+                    goto end_of_parse;
+
+            default:
+                textBuffer[index] = *this->cursorPtr;
+                textBuffer[index + 1] = *(this->cursorPtr + 1);
+                index += 2;
+                this->cursorPtr += 2;
+                break;
+
             } // switch(*this->cursorPtr)
-        } // for(;;)
-    } // if(this->timer3 <= 0)
-
-    this->timer2--;
-    if(this->unk2a8c == 0){
-        if(WAS_PRESSED(0x1001) || (this->canSkip != 0 && IS_PRESSED(0x100))){
-            this->timer2 = 0;
-        }
-    }
-    else {
-        this->unk2a8c--;
-    }
-
+        } // while(true)
+    
 end_of_parse:
     this->timer1++;
     this->unk0c -= this->unk10;
